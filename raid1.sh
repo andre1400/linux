bfb384ef9227e04bb376d3f3998da875aa1a1eab
andre1400 <115775692+andre1400@users.noreply.github.com>
2022-11-05 12:20:27 +0100

    Create raid1.sh

---
+++ raid1.sh
@@ -0,0 +1,80 @@
+# RAID 1 - Configuration et Installation
+
+Pré-requis : 
+- au moins 2 disques durs de même taille
+- Droits Sudo / Root 
+
+#### Afficher les disques et partitions
+```bash
+lsblk
+```
+
+#### Installer le paquet mdadm 
+```bash
+sudo apt install mdadm -y
+```
+
+#### Création du volume logique raid 1, sélectionner le nombre de disque et leur noms
+
+````bash
+sudo mdadm --create --verbose /dev/md5 --level=1 --raid-devices=2 /dev/nvme1n1 /dev/nvme2n1
+````
+
+#### Création du système de fichier .ext4 dans le volume logique md5
+
+````bash
+sudo mkfs.ext4 -F /dev/md5
+````
+
+#### Monter le système de fichier
+
+````bash
+sudo mount /dev/md5 /mnt
+````
+
+#### Attribution des droits dans le dossier /mnt
+
+````bash
+sudo chmod o+w /mnt
+````
+
+#### Vérifier l'espace du disque (Optionnel)
+
+````bash
+sudo df -hT
+````
+
+## Sauvegarder la pertinence du raid
+
+
+
+#### Cette commande scan et ajoute le log dans le fichier  le volume logique automatiquement à chaque boot
+
+````bash
+sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf
+````
+
+#### Mettre a jour l'initramfs afin de garder la configuration du RAID après un reboot
+
+````bash
+sudo update-initramfs -u
+````
+
+#### Ajoute le système de fichier monté dans le fichier /etc/fstab pour un montage automatique lors du boot.
+
+````bash
+echo '/dev/md5 /mnt/md5 ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab
+````
+
+#### Redémarre la machine linux
+
+````bash
+sudo reboot
+````
+
+#### Vérification de l'état du RAID 1
+
+```
+sudo cat /proc/mdstat
+```
+
